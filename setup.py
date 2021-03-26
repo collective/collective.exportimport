397cf6f49f77e238e3841dd490f0afa6a2d9d901
Philip Bauer <bauer@starzel.de>
2021-03-26 18:42:45 +0100

    declare beta

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 2 - Beta",
         "Intended Audience :: Developers",
         "Environment :: Web Environment",
         "Framework :: Plone",
