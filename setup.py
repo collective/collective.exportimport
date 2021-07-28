9e7ba47be4286d6071bf6802e53d8c01336ea7d2
Philip Bauer <bauer@starzel.de>
2021-07-28 09:04:32 +0200

    allow 3.6

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ setup(
     package_dir={"": "src"},
     include_package_data=True,
     zip_safe=False,
-    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*, !=3.6.*",
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*",
     install_requires=install_requires,
     extras_require={
         "test": [
