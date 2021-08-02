dad96ae4945bde4c8070331256c378c456ec5a96
Philip Bauer <bauer@starzel.de>
2021-08-02 12:32:55 +0200

    support 3.9

--- setup.py
+++ setup.py
@@ -49,6 +49,7 @@ setup(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Operating System :: OS Independent",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
     ],
