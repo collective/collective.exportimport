fb3683fe735e4462fd5f34e38a531f550a9c33e3
Philip Bauer <bauer@starzel.de>
2021-09-02 19:53:31 +0200

    fix download in py3 and simplify stupid dance around the last comma

--- src/collective/exportimport/export_content.py
+++ src/collective/exportimport/export_content.py
@@ -120,13 +120,12 @@ class ExportContent(BrowserView):
             cfg = getConfiguration()
             filepath = os.path.join(cfg.clienthome, filename)
             with open(filepath, 'w') as f:
-                f.write('[')
                 for number, datum in enumerate(content_generator, start=1):
+                    if number == 1:
+                        f.write('[')
+                    else:
+                        f.write(',')
                     json.dump(datum, f, sort_keys=True, indent=4)
-                    f.write(',')
-                # remove last comma to create valid json
-                f.seek(-1, os.SEEK_END)
-                f.truncate()
                 f.write(']')
             msg = u"Exported {} {} as {} to {}".format(number, self.portal_type, filename, filepath)
             logger.info(msg)
@@ -137,14 +136,13 @@ class ExportContent(BrowserView):
                 noLongerProvides(self.request, IBase64BlobsMarker)
             self.request.response.redirect(self.request['ACTUAL_URL'])
         else:
-            with tempfile.TemporaryFile() as f:
-                f.write('[')
+            with tempfile.TemporaryFile(mode="w+") as f:
                 for number, datum in enumerate(content_generator, start=1):
+                    if number == 1:
+                        f.write('[')
+                    else:
+                        f.write(',')
                     json.dump(datum, f, sort_keys=True, indent=4)
-                    f.write(',')
-                # remove last comma to create valid json
-                f.seek(-1, os.SEEK_END)
-                f.truncate()
                 f.write(']')
                 msg = u"Exported {} {}".format(number, self.portal_type)
                 logger.info(msg)
@@ -160,7 +158,7 @@ class ExportContent(BrowserView):
                     # remove marker interface
                     noLongerProvides(self.request, IBase64BlobsMarker)
                 f.seek(0)
-                return response.write(f.read())
+                return response.write(safe_bytes(f.read()))
 
     def build_query(self):
         query = {"portal_type": self.portal_type, "sort_on": "path"}
