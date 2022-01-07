873c9bb018cd9c021565fbe51670332f8176e37e
Philip Bauer <bauer@starzel.de>
2022-01-07 12:09:42 +0100

    Back to development: 1.5

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ else:
 
 setup(
     name="collective.exportimport",
-    version="1.4",
+    version="1.5.dev0",
     description="An add-on for Plone to Export and import content, members, relations, translations and localroles.",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
