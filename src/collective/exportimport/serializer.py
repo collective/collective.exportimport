4d21cfeb770d3e432cb37d656f25b8ff4507ba82
Thibaut Born <thibaut.born@kuleuven.be>
2022-05-17 15:36:52 +0200

    export topic sort criteria

--- src/collective/exportimport/serializer.py
+++ src/collective/exportimport/serializer.py
@@ -391,6 +391,10 @@ if HAS_AT and HAS_PAC:
             if self.context.itemCount:
                 topic_metadata["b_size"] = self.context.itemCount
 
+            if hasattr(self, "_collection_sort_on"):
+                topic_metadata["sort_on"] = self._collection_sort_on
+                topic_metadata["sort_reversed"] = self._collection_sort_reversed
+
             return topic_metadata
 
 
