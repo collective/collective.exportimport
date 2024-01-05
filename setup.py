51e5387d9237a51110001ddd012227bea924dc6b
Johannes Raggam <thetetet@gmail.com>
2024-01-05 19:18:58 +0100

    Sort requirements.

--- setup.py
+++ setup.py
@@ -16,26 +16,26 @@ long_description = "\n\n".join(
 )
 
 install_requires = [
-    "setuptools",
-    "plone.api >= 1.8.4",
     "hurry.filesize",
     "ijson",
+    "plone.api >= 1.8.4",
+    "setuptools",
     "six",
 ]
 
 if sys.version_info[0] < 3:
     install_requires.append("beautifulsoup4 < 4.10")
-    install_requires.append("plone.restapi < 8.0.0")
-    # There is a py2-imcompatibility in plone.rest 3.0.1
-    install_requires.append("plone.rest < 3.0.1")
     # plone.restapi depends on plone.schema, which depends on jsonschema,
     # which has a Py3-only release since September 2021.
     install_requires.append("jsonschema < 4")
-    install_requires.append("pyrsistent < 0.16.0")
     install_requires.append("pathlib2")
+    # There is a py2-imcompatibility in plone.rest 3.0.1
+    install_requires.append("plone.rest < 3.0.1")
+    install_requires.append("plone.restapi < 8.0.0")
+    install_requires.append("pyrsistent < 0.16.0")
 else:
-    install_requires.append("plone.restapi")
     install_requires.append("beautifulsoup4")
+    install_requires.append("plone.restapi")
 
 
 setup(
