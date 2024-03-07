7a1532260a69d116ad35f9012270a9d52214a93a
Philip Bauer <bauer@starzel.de>
2024-03-07 13:08:57 +0100

    Fix migrating bocks by bypassing the frontend-specific modifiers when serializing and deserializing blocks

--- src/collective/exportimport/deserializer.py
+++ src/collective/exportimport/deserializer.py
@@ -3,8 +3,11 @@ from plone.app.textfield.interfaces import IRichText
 from plone.app.textfield.value import RichTextValue
 from plone.dexterity.interfaces import IDexterityContent
 from plone.portlets.interfaces import IPortletAssignment
+from plone.restapi.behaviors import IBlocks
 from plone.restapi.deserializer.dxfields import DefaultFieldDeserializer
 from plone.restapi.interfaces import IFieldDeserializer
+from plone.restapi.interfaces import IFieldDeserializer
+from plone.schema import IJSONField
 from zope.component import adapter
 from zope.interface import implementer
 
@@ -15,6 +18,7 @@ class RichTextFieldDeserializerWithoutUnescape(DefaultFieldDeserializer):
     """Override default RichTextFieldDeserializer without using html_parser.unescape().
     Fixes https://github.com/collective/collective.exportimport/issues/99
     """
+
     def __call__(self, value):
         content_type = self.field.default_mime_type
         encoding = "utf8"
@@ -38,3 +42,11 @@ class RichTextFieldDeserializerWithoutUnescape(DefaultFieldDeserializer):
 @adapter(IRichText, IPortletAssignment, IMigrationMarker)
 class PortletRichTextFieldDeserializer(RichTextFieldDeserializerWithoutUnescape):
     pass
+
+
+@implementer(IFieldDeserializer)
+@adapter(IJSONField, IBlocks, IMigrationMarker)
+class ImportingBlocksJSONFieldDeserializer(DefaultFieldDeserializer):
+    """We skip the subscribers that deserialize the blocks from the frontend.
+    We only need the raw data.
+    """
